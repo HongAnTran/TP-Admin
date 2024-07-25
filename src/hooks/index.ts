e6b6dcb0e1c71ef47b6c3d48e5beffa4b4f5e1cf
hongantran <hongan.tran@beme.vn>
2024-07-25 13:37:58 +0700

    up

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+import useGetDoc from "./usegetDoc";
+import useAuthContext from "./useAuthContext";
+import useGetDocRealTime from "./useGetDocRealTime";
+export { useGetDoc, useAuthContext   , useGetDocRealTime}
